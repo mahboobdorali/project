59577032624b6867129e4eae427bc1dc92441cef
mahboob <mahboobdorali@gmail.com>
2023-01-19 12:51:04 +0330

    change name of order class to OrdersCustomer

--- src/main/java/ir/maktab/madel/entity/OrdersCustomer.java
+++ src/main/java/ir/maktab/madel/entity/OrdersCustomer.java
@@ -2,7 +2,7 @@ package ir.maktab.madel.entity;
 
 import ir.maktab.madel.enumurated.CurrentSituation;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -13,19 +13,17 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Order {
+@Builder
+@NamedQueries(
+        @NamedQuery(name = "getAllOrder",query = "FROM OrdersCustomer "))
+public class OrdersCustomer {//سفارش
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private double proposedPrice;
-
     private String jobDescription;
-
-
     @Temporal(value = TemporalType.TIMESTAMP)
     private Date dateAndTimeOfWork;
-
     @Column(nullable = false)
     private String customerAddress;
 
